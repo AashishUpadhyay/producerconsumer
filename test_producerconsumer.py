9ca6525b3fae93e2d5dc0e17c112245ce60d6e2a
Aashish Upadhyay <aupadhyay@gmail.com>
2021-04-24 16:47:37 -0400

    First Draft

---
+++ test_producerconsumer.py
@@ -0,0 +1,28 @@
+from producerconsumer import *
+import os
+
+def test_build_projects_queue():
+    pipeline = Pipeline()
+    q = pipeline._build_projects_queue(10,100)
+    assert q.qsize() == 10
+
+def test_consumer_createcsv_smoke_test():
+    q = Queue.Queue()
+    sc = SynchronizationContext()
+    consumer = Consumer(q, sc)
+    consumer.createcsv("asdf")
+    assert True
+    os.remove("asdf")
+
+def test_producer_fetchprojectandtasks():
+    sq = Queue.Queue()
+    tq = Queue.Queue()
+    sc = SynchronizationContext()
+    producer = Producer(sq, tq, sc)
+    start, end = producer.fetchprojectandtasks((1,100))
+    assert start == 0
+    assert end == 100
+
+def test_pipeline_build_and_run():
+    pipeline = Pipeline()
+    pipeline.build_and_run(10, 10)
